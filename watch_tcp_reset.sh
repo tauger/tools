5ea109ef058ca0c3a6cee392b23f15ffdb4176d3
Grant Zhang <gzhang@fastly.com>
2015-07-30 18:57:24 +0000

    Add kprobe for tcp6 and clean up after testing.

--- watch_tcp_reset.sh
+++ watch_tcp_reset.sh
@@ -2,5 +2,9 @@
 
 echo 'p:kprobes/tcp_send_active_reset tcp_send_active_reset' >> /sys/kernel/debug/tracing/kprobe_events
 echo 'p:kprobes/tcp_v4_send_reset tcp_v4_send_reset' >> /sys/kernel/debug/tracing/kprobe_events
+echo 'p:kprobes/tcp_v6_send_reset tcp_v6_send_reset' >> /sys/kernel/debug/tracing/kprobe_events
 
 perf stat -a -e "kprobes:*" sleep $1
+
+# clean up these kprobes
+perf probe -d "kprobes:*"
